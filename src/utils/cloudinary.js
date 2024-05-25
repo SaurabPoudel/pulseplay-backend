4e49b7badcaaa980db009b5b0d4b69e846c9cb59
SaurabPoudel <poudelsaurab20@gmail.com>
2024-05-25 13:37:39 +0545

    multer cloudinary config

---
+++ src/utils/cloudinary.js
@@ -0,0 +1,28 @@
+import { v2 as cloudinary } from "cloudinary";
+import fs from "fs";
+
+// Configuration
+cloudinary.config({
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLODINARY_API_SECRET, // Click 'View Credentials' below to copy your API secret
+});
+
+const uploadOnCloudinary = async (localFilePath) => {
+  try {
+    if (!localFilePath) return null;
+    // upload the file on cloudinary
+    const response = await cloudinary.uploader.upload(localFilePath, {
+      resource_type: "auto",
+    });
+    // after succesfull upload of file
+    console.log("File is uploaded on cloudinary", response.url);
+    return response;
+  } catch (error) {
+    // remove the locally save temp file when upload operation fails
+    fs.unlinkSync(localFilePath);
+    return null;
+  }
+};
+
+export { uploadOnCloudinary };
